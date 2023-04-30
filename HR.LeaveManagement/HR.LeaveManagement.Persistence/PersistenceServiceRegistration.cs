45b4e14b7990ecfe330e50de5131a1a6f0cc9504
Dawit Teshager <dawit201121@gmail.com>
2023-04-30 16:32:27 +0300

    update unit test

---
+++ HR.LeaveManagement/HR.LeaveManagement.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,31 @@
+﻿using HR.LeaveManagement.Application.Contracts.Persistence;
+using LeaveManagement.Persistence.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace HR.LeaveManagement.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+
+        public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            
+
+            services.AddEntityFrameworkNpgsql()
+            .AddDbContext<LeaveManagementDbContext>(opt =>
+            opt.UseNpgsql(configuration.GetConnectionString("LeaveManagementConnectionString")));
+
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>)) ;
+            services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
+            services.AddScoped<ILeaveRequestRepository, LeaveRequestRepository>();
+            services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
+
+            return services;
+        }
+
+
+    }
+}
